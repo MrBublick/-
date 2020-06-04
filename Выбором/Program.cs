4e3385380a99dd18a18136d1b1fbb7d4d1215f7f
MrBublick <46197274+MrBublick@users.noreply.github.com>
2020-06-04 15:58:35 +0700

    F

--- Выбором/Program.cs
+++ Выбором/Program.cs
@@ -10,6 +10,49 @@ namespace Выбором
     {
         static void Main(string[] args)
         {
+            Console.Write("Ввести количество элементов: ");
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            //Заполнение массива и вывод начального массива
+            Random ran = new Random();
+            Console.Write("Исходный массив: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = ran.Next(1, 100);
+                Console.Write("  {0}", array[i]);
+            }
+            Console.WriteLine();
+            //Сортировка и ввыод массива
+            Console.WriteLine("Упорядоченный массив: {0}", string.Join("  ", СhoiceSort(array)));
+
+            Console.ReadLine();
         }
+
+
+        //сортировка выбром
+        static int[] СhoiceSort(int[] array)
+        {
+            int min = 0, temp = 0;
+            for (int i = 0; i < 5 - 1; i++)
+            {
+                min = i;
+
+                for (int j = i + 1; j < 5; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                temp = array[i];
+                array[i] = array[min];
+                array[min] = temp;
+
+            }
+            return array;
+        }
+        
     }
 }
